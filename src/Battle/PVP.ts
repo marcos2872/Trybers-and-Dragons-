797a7152c467e25c0ac565ff16c1d30e485dd555
marcos <njsu5@hotmail.com>
2022-11-18 22:17:48 -0300

    req 10

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,32 @@
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  
+  private _fighter1: Fighter;
+  private _fighter2: Fighter;
+  constructor(fighter1: Fighter, fighter2: Fighter) {
+    super(fighter1);
+    this._fighter1 = fighter1;
+
+    this._fighter2 = fighter2;
+  }
+
+  fight(): number {
+    let fighter = 0;
+
+    while (
+      this._fighter1.lifePoints !== -1
+      && this._fighter2.lifePoints !== -1
+    ) {
+      this._fighter2.attack(this._fighter1);
+      this._fighter1.attack(this._fighter2);
+
+      if (this._fighter1.lifePoints === -1) {
+        fighter = -1;
+      } else {
+        fighter = 1;
+      }
+    }
+    return fighter;
+  }
 }
--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -2,8 +2,6 @@ import Energy from '../Energy';
 import SimpleFighter from './SimpleFighter';
 
 export default interface Fighter extends SimpleFighter {
-  lifePoints: number;
-  strength: number;
   defense: number;
   energy?: Energy;
 
